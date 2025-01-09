397b11159ec8eb38e2a269f3df1e1e1ba0b66df3
KritikaJarori <113189927+KritikaJarori@users.noreply.github.com>
2025-01-09 18:42:40 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Login | Voting System</title>
+ 	
+
+<?php include('./header.php'); ?>
+<?php 
+session_start();
+if(isset($_SESSION['login_id']))
+header("location:index.php?page=home");
+?>
+
+</head>
+<style>
+	body{
+		width: 100%;
+	    height: calc(100%);
+	    /*background: #007bff;*/
+	}
+	main#main{
+		width:100%;
+		height: calc(100%);
+		background:white;
+	}
+	#login-right{
+		position: absolute;
+		right:0;
+		width:40%;
+		height: calc(100%);
+		background:white;
+		display: flex;
+		align-items: center;
+		background-color: red;
+	}
+	#login-left{
+		position: absolute;
+		left:0;
+		width:60%;
+		height: calc(100%);
+		background:white;
+		display: flex;
+		align-items: center;
+	}
+	#login-right .card{
+		margin: auto
+	}
+	.logo {
+    margin: auto;
+    font-size: 8rem;
+    /*background: #00000061;*/
+    /*padding: .5em 0.8em;*/
+    /*border-radius: 50% 50%;*/
+	color: white;  /*color: #000000b3;*/
+}
+#title{
+	position :absolute;
+	display: flex;
+	top : 125px;
+	left: 40px;
+	align-items: center;
+	text-align: center;
+}
+</style>
+
+<body>
+
+
+  <main id="main" class=" alert-info">
+  		<div id="login-left">
+  			<div class="logo">
+  				<!--<h1>CHMSC Online Voting System</h1>
+  				<img src="chmsc.png" alt="..." width="100%">-->
+				<h1>LNCTS Online Voting System</h1>
+  				<img src="lncts_logo.png" alt="..." width="100%">
+  			</div>
+  		</div>
+  		<div id="login-right" class="bg-warning">
+			<div id="title"> <h1>LNCTS Online Voting System</h1></div>
+  			<div class="card col-md-8">
+  				<div class="card-body">
+  					<form id="login-form" >
+  						<div class="form-group">
+  							<label for="username" class="control-label">Username</label>
+  							<input type="text" id="username" name="username" class="form-control">
+  						</div>
+  						<div class="form-group">
+  							<label for="password" class="control-label">Password</label>
+  							<input type="password" id="password" name="password" class="form-control">
+  						</div>
+  						<center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
+  					</form>
+  				</div>
+  			</div>
+  		</div>
+   
+
+  </main>
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+
+</body>
+<script>
+	$('#login-form').submit(function(e){
+		e.preventDefault()
+		$('#login-form button[type="button"]').attr('disabled',true).html('Logging in...');
+		if($(this).find('.alert-danger').length > 0 )
+			$(this).find('.alert-danger').remove();
+		$.ajax({
+			url:'ajax.php?action=login',
+			method:'POST',
+			data:$(this).serialize(),
+			error:err=>{
+				console.log(err)
+		$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+
+			},
+			success:function(resp){
+				if(resp == 1){
+					location.href ='index.php?page=home';
+				}else if(resp == 2){
+					location.href ='voting.php';
+				}else{
+					$('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
+					$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+				}
+			}
+		})
+	})
+</script>	
+</html>
